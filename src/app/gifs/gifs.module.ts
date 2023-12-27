380283df7762dda4c06210a24f31c01b9cd57c1c
Antía <elyosis@proton.me>
2023-12-27 19:52:09 +0100

    Added Homepage component

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomepageComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HomepageComponent]
 })
 export class GifsModule { }
