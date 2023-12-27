a5c869a38c74837545465327d35c8efc62235541
Antía <elyosis@proton.me>
2023-12-27 20:23:43 +0100

    Added card list component

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    SearchBarComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule
