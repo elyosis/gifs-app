616b193eef20565497912a6ba9154ea965de3c4a
Antía <elyosis@proton.me>
2023-12-28 16:48:57 +0100

    Connected search bar to service to log search history

--- src/app/gifs/components/search-bar/search-bar.component.ts
+++ src/app/gifs/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-bar',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
+  @ViewChild("tagInput")
+  tagInput!: ElementRef<HTMLInputElement>
 
+  constructor(private gifsService: GifsService) {}
+
+  logTag():void {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.addTag(newTag);
+    this.tagInput.nativeElement.value = "";
+  }
 }
