944417f7a04619dbd8548b8cde521e1d610e06a1
Antía <elyosis@proton.me>
2023-12-28 18:40:17 +0100

    Added http request to obtain gif data

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,25 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gifs, SearchResults } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  public gifsList: Gifs[] = [];
   private _tagsHistory: string[] = [];
+  // TODO: delete key once the app's done
+  private apiKey: string = "Bd744kPTO3Ouh7vZAYYGbRlpt0bBknle";
+  private url: string = "https://api.giphy.com/v1/gifs/search";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  organizeTagsHistory(newTag: string):void {
+  organizeTagsHistory(newTag: string): void {
     if (this._tagsHistory.includes(newTag)) {
       this._tagsHistory = this._tagsHistory.filter(tag => tag !== newTag);
     }
@@ -23,10 +29,17 @@ export class GifsService {
   }
 
   addTag(newTag: string): void {
-    if (newTag.length === 0) {
-      return;
-    }
-
+    if (newTag.length === 0) { return }
     this.organizeTagsHistory(newTag);
+
+    const params = new HttpParams()
+      .set("api_key", this.apiKey)
+      .set("q", newTag)
+      .set("limit", 10)
+
+    this.http.get<SearchResults>(this.url, { params })
+      .subscribe(res => {
+        this.gifsList = res.data;
+      })
   }
 }
