ecc496da249a28da250c40d95f798fccc9fc9dcd
Antía <elyosis@proton.me>
2023-12-29 00:39:26 +0100

    Added loading animation for card images

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { CardListComponent } from './components/card-list/card-list.component';
@@ -16,7 +18,8 @@ import { CardComponent } from './components/card/card.component';
     CardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [HomepageComponent]
 })
--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrls: ['./lazy-image.component.css']
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+
+  @Input() url!: string;
+  @Input() altText: string = "";
+  hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) {
+      throw new Error("The URL property is required");
+    }
+  }
+
+  onLoad():void {
+    this.hasLoaded = true;
+  }
 
 }
